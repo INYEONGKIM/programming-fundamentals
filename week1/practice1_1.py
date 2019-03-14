5a6f88903ad9f4aee4a99b27eadc0841007ea22b
INYEONGKIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-03-14 21:26:21 +0900

    Add files via upload

---
+++ week1/practice1_1.py
@@ -0,0 +1,4 @@
+print("해와 달 수를 날짜 수로 환산해드립니다.")
+year=int(input("몇 년? "))
+month=int(input("몇 개월? "))
+print(year,"년",month,"개월은 날짜로 환산하면",365*year+month*30,"일입니다.")
---
+++ week1/practise1_3.py
@@ -0,0 +1,3 @@
+print("화씨온도를 섭씨로 바꿔 줄께")
+f=float(input("화씨로 몇 도?"))
+print("섭씨로",round((f-32)*float(5/9),1),"도")
