bf2351d08fb6ab15dcb3ef4d72132880929556fe
INYEONG KIM <42140395+INYEONGKIM@users.noreply.github.com>
2019-04-22 12:16:39 +0900

    Update homework6_1.py

--- week6/homework6_1.py
+++ week6/homework6_1.py
@@ -23,7 +23,6 @@ def minsteps(n):
             memo[i] = min(memo[i], memo[i // 2]+1)
         if i % 3 == 0:
             memo[i] = min(memo[i], memo[i // 3]+1)
-    # print(memo[1:])
     return memo[n]-1
 
 print(minsteps(10)) # 3
